3a463fc357f0bd56514c47bac2eeb0969d78e5f8
adhishyam <76176206+adhishyam@users.noreply.github.com>
2020-12-23 16:17:33 -0600

    Update DataCleaning_KaggleSurvey.R

--- DataCleaning_KaggleSurvey.R
+++ DataCleaning_KaggleSurvey.R
@@ -279,4 +279,32 @@ dat2<- (dat1
 )
 
 
+country<- (dat2
+           %>% group_by(country)
+           %>% summarize(freq = n())
+           %>% mutate(country = ifelse(country == "United States of America","USA",
+                                       ifelse(country == "United Kingdom of Great Britain and Northern Ireland", "UK",
+                                              ifelse(country== "Iran, Islamic Republic of...", "Iran", country))))
+)
 
+
+
+
+names(country)<- c("region", "freq")
+
+WorldData <- map_data('world') %>% filter(region != "Antarctica") %>% fortify
+
+p <- ggplot() +
+  geom_map(data = WorldData, map = WorldData,
+           aes(x = long, y = lat, group = group, map_id=region),
+           fill = "white", colour = "#7f7f7f", size=0.5) + 
+  geom_map(data = country, map=WorldData,
+           aes(fill=freq, map_id=region),
+           colour="#7f7f7f", size=0.5) +
+  coord_map("rectangular", lat0=0, xlim=c(-180,180), ylim=c(-60, 90)) +
+  scale_fill_continuous(low="thistle2", high="darkred", guide="colorbar") +
+  scale_y_continuous(breaks=c()) +
+  scale_x_continuous(breaks=c()) +
+  labs(fill="legend", title="Kaggle Participants by country", x="", y="") +
+  theme_bw()
+p 
